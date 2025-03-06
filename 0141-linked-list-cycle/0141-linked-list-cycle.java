e2df1d77eb44099842664f226c0bb8f47af0182f
Rupak Das <rupak.das.3002@gmail.com>
2025-03-07 00:59:09 +0530

    Time: 0 ms (100%), Space: 44.6 MB (38.3%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        ListNode hare = head;
+        ListNode tort = head;
+        while(hare != null && hare.next != null) {
+            hare = hare.next.next;
+            tort = tort.next;
+            if(hare == tort)
+                return true;
+        }
+        return false;
+    }
+}
